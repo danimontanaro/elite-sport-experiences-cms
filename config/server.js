ec422926764d189e8e3fa05c21a79b3f627f3fb3
Daniela Montanaro <dani.montanaro21@gmail.com>
2020-10-01 10:38:33 -0300

    initializing strapi project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9f3b7569ff4c774e9cff95619726e7be'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0d7e29a6-96f5-4888-b9f0-7c5beec1398e'
+};
